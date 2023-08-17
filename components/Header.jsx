6a653a019a429958e042b5f0d10fb2997f5a385c
Mohammed <sajidcurious9@gmail.com>
2023-08-18 04:01:35 +0530

    making header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>
+        <Link href="/">
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" alt="" />
+        </Link>
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
