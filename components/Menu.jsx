028876d71edf0bb857d3ab7ca333a3978c6b9e56
Mohammed <sajidcurious9@gmail.com>
2023-08-19 00:57:42 +0530

    added nav data

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+const data = [
+  { id: 1, name: "Home", url: "/" },
+  { id: 2, name: "About", url: "/about" },
+  { id: 3, name: "Categories", subMenu: true },
+  { id: 4, name: "Contact", url: "/contact" },
+];
+
+const subMenuData = [
+  { id: 1, name: "Jordan", doc_count: 11 },
+  { id: 2, name: "Sneakers", doc_count: 8 },
+  { id: 3, name: "Running shoes", doc_count: 64 },
+  { id: 4, name: "Football shoes", doc_count: 107 },
+];
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+      {data.map((item) => {
+        return <React.Fragment key={item.id}></React.Fragment>;
+      })}
+    </ul>
+  );
 };
 
 export default Menu;
