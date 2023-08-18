1c3ea0046095a07950bf29d8f466f12613b73393
Mohammed <sajidcurious9@gmail.com>
2023-08-19 01:37:42 +0530

    added navlinks and navs

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { BsChevronDown } from "react-icons/bs";
 
 const data = [
   { id: 1, name: "Home", url: "/" },
@@ -14,11 +16,42 @@ const subMenuData = [
   { id: 4, name: "Football shoes", doc_count: 107 },
 ];
 
-const Menu = () => {
+const Menu = ({ showCatMenu, setShowCatMenu }) => {
   return (
     <ul className="hidden md:flex items-center gap-8 font-medium text-black">
       {data.map((item) => {
-        return <React.Fragment key={item.id}></React.Fragment>;
+        return (
+          <React.Fragment key={item.id}>
+            {!!item?.subMenu ? (
+              <li
+                className="cursor-pointer flex items-center gap-2 relative"
+                onMouseEnter={() => setShowCatMenu(true)}
+                onMouseLeave={() => setShowCatMenu(false)}
+              >
+                {item.name}
+                <BsChevronDown size={14} />
+                {showCatMenu && (
+                  <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1  text-black shadow-lg">
+                    {subMenuData.map((submenu) => {
+                      return (
+                        <Link href="/" key={submenu.id}>
+                          <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                            {submenu.name}
+                          </li>
+                          <span className="opacity-50 text-sm">78</span>
+                        </Link>
+                      );
+                    })}
+                  </ul>
+                )}
+              </li>
+            ) : (
+              <li>
+                <Link href={item?.url}>{item.name}</Link>
+              </li>
+            )}
+          </React.Fragment>
+        );
       })}
     </ul>
   );
