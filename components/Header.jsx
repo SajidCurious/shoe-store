de54af1e8c0c75fbc05f2ede4ca29d577268a3ef
Mohammed <sajidcurious9@gmail.com>
2023-08-19 01:57:16 +0530

    added right icons they are liked and cart icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Menu from "./Menu";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -13,11 +17,30 @@ const Header = () => {
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
     >
-      <Wrapper>
+      <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
           <img src="/logo.svg" className="w-[40px] md:w-[60px]" alt="" />
         </Link>
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+
+        <div className="flex items-center gap-2 text-black">
+          {/* icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.06] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              50
+            </div>
+          </div>
+          {/* icon end */}
+          {/* icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.06] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>
+          </div>
+          {/* icon end */}
+        </div>
       </Wrapper>
     </header>
   );
