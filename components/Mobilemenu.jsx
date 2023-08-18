915d778e98df7dd4daea8f65d033d3f44380f0ef
Mohammed <sajidcurious9@gmail.com>
2023-08-19 03:08:18 +0530

    added the mobile nav component

---
+++ components/Mobilemenu.jsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+
+const data = [
+  { id: 1, name: "Home", url: "/" },
+  { id: 2, name: "About", url: "/about" },
+  { id: 3, name: "Categories", subMenu: true },
+  { id: 4, name: "Contact", url: "/contact" },
+];
+
+const subMenuData = [
+  { id: 1, name: "Jordan", doc_count: 11 },
+  { id: 2, name: "Sneakers", doc_count: 8 },
+  { id: 3, name: "Running shoes", doc_count: 64 },
+  { id: 4, name: "Football shoes", doc_count: 107 },
+];
+
+const Mobilemenu = ({ showCatMenu, setShowCatMenu, setMobileMenu }) => {
+  return (
+    <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+      {data.map((item) => {
+        return (
+          <React.Fragment key={item.id}>
+            {!!item?.subMenu ? (
+              <li
+                className="cursor-pointer flex items-center gap-2 relative"
+                onMouseEnter={() => setShowCatMenu(true)}
+                onMouseLeave={() => setShowCatMenu(false)}
+              >
+                <div>
+                  {item.name}
+                  <BsChevronDown size={14} />
+                </div>
+                {showCatMenu && (
+                  <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1  text-black shadow-lg">
+                    {subMenuData.map((submenu) => {
+                      return (
+                        <Link
+                          href="/"
+                          key={submenu.id}
+                          onClick={() => setShowCatMenu(false)}
+                        >
+                          <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                            {submenu.name}
+                          </li>
+                          <span className="opacity-50 text-sm">78</span>
+                        </Link>
+                      );
+                    })}
+                  </ul>
+                )}
+              </li>
+            ) : (
+              <li>
+                <Link href={item?.url}>{item.name}</Link>
+              </li>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Mobilemenu;
