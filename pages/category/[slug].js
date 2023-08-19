8e4b6a102d4e999f953b430d6563dff421d3bd60
Mohammed <sajidcurious9@gmail.com>
2023-08-20 03:13:32 +0530

    created category folder inside the pages for dynamic routing and added routing code

---
+++ pages/category/[slug].js
@@ -0,0 +1,27 @@
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+import ProductCard from "@/components/ProductCard";
+
+const category = () => {
+  return (
+    <div>
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Running Shoes
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default category;
