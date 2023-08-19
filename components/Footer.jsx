e2fe63aaacd1d26327d6b4bbddfcc006b79171b2
Mohammed <sajidcurious9@gmail.com>
2023-08-20 01:10:03 +0530

    added social links to the icons in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 import Wrapper from "./Wrapper";
 
 const Footer = () => {
@@ -81,22 +82,28 @@ const Footer = () => {
         {/* RIGHT START */}
         <div className="flex gap-4 justify-center md:justify-start">
           <div
-            onClick={() => window.open("https://facebook.com", "_blank")}
-            className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/sajidcurious/", "_blank")
+            }
+            className="w-10 h-10 rounded-full bg-white flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
           >
-            <FaFacebookF size={20} />
+            <BsLinkedin size={20} />
           </div>
-          <Link
-            href="https://twitter.com"
-            className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
+          <div
+            onClick={() =>
+              window.open("https://twitter.com/sajid_curious", "_blank")
+            }
+            className="w-10 h-10 rounded-full bg-white flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
           >
             <FaTwitter size={20} />
-          </Link>
-          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-            <FaYoutube size={20} />
           </div>
-          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-            <FaInstagram size={20} />
+          <div
+            onClick={() =>
+              window.open("https://github.com/SajidCurious", "_blank")
+            }
+            className="w-10 h-10 rounded-full bg-white flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
+          >
+            <BsGithub size={20} />
           </div>
         </div>
         {/* RIGHT END */}
