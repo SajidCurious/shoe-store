4a0a1ba7677a5876ff3d7d2429f6571d5a575b92
Mohammed <sajidcurious9@gmail.com>
2023-08-20 02:22:08 +0530

    added heading and paragraph below the HeroBanner section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <>
       <main>
         <HeroBanner />
+        <Wrapper>
+          {" "}
+          {/* heading and paragaph start */}
+          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+              Cushioning for Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              A lightweight Nike ZoomX midsole is combined with increased stack
+              heights to help provide cushioning during extended stretches of
+              running.
+            </div>
+          </div>
+          {/* heading and paragaph end */}
+        </Wrapper>
       </main>
     </>
   );
