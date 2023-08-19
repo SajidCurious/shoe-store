287a8eb3e18732855739b0cbaeff266ee21f58c1
Mohammed <sajidcurious9@gmail.com>
2023-08-20 01:47:55 +0530

    added HeroBanner slider with the help of react responsive slider and customized it

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        renderArrowPrev={(clickHandler, hasPrev) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiLeftArrowAlt className="text-sm md:text-lg text-white" />
+          </div>
+        )}
+        renderArrowNext={(clickHandler, hasNext) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiRightArrowAlt className="text-sm md:text-lg text-white" />
+          </div>
+        )}
+      >
+        <div>
+          <img src="/slide-1.png" />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+            Shop now
+          </div>
+        </div>
+        <div>
+          <img src="/slide-2.png" />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+            Shop now
+          </div>
+        </div>
+        <div>
+          <img src="/slide-3.png" />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+            Shop now
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroBanner;
