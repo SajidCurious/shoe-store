153cbae560e65e702686752ea022072858747e07
Mohammed <sajidcurious9@gmail.com>
2023-08-20 02:11:56 +0530

    edited logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
             {/* MENU START */}
             <div className="flex flex-col gap-3">
               <div className="font-oswald font-medium uppercase text-sm">
-                About nike
+                About
               </div>
               <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
                 News
@@ -111,7 +111,7 @@ const Footer = () => {
       <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
         {/* LEFT START */}
         <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
-          © 2023 Nike, Inc. All Rights Reserved
+          © 2023 SHOOZIE, Inc. All Rights Reserved
         </div>
         {/* LEFT END */}
 
