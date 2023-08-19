92600fdd5b9ac129561b4b5139f9707bdaf5a5b3
Mohammed <sajidcurious9@gmail.com>
2023-08-20 02:46:14 +0530

    imported the productCard into the index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
@@ -20,6 +21,14 @@ export default function Home() {
             </div>
           </div>
           {/* heading and paragaph end */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
         </Wrapper>
       </main>
     </>
