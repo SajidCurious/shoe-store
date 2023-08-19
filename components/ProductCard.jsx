e828b08d124dc8481d256daa9f120eb33246e0fe
Mohammed <sajidcurious9@gmail.com>
2023-08-20 02:45:27 +0530

    added product card component and completed ite

---
+++ components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+      <Link href={/product/}>
+        <img src="/product-1.webp" alt="Product Image" className="w-full" />
+        <div className="p-4 text-black/[0.9]">
+          <h2 className="text-lg font-medium">Product Name</h2>
+          <div className="flex items-center text-black/[0.5]">
+            <p className="mr-2 text-lg font-semibold">$20.00</p>
+            <p className="text-base font-medium line-through">$25.00</p>
+            <p className="ml-auto text-base font-medium text-green-500">
+              20% off
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductCard;
