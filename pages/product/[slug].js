8b7c3e5f065ff5e88cfe29d9ece38e5ac92346e7
Mohammed <sajidcurious9@gmail.com>
2023-08-22 01:32:27 +0530

    added product details page and building it

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,12 @@
+import Wrapper from "@/components/Wrapper";
 import React from "react";
 
 const productDetails = () => {
-  return <div>productDetails</div>;
+  return (
+    <div className="w-full md:py-20">
+      <Wrapper></Wrapper>
+    </div>
+  );
 };
 
 export default productDetails;
