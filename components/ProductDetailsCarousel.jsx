7a1440cb01dba051186d56d6ae4c5e2281cdf852
Mohammed <sajidcurious9@gmail.com>
2023-08-22 02:01:31 +0530

    completed carousel component for the product details page using react-responsive-slider

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const ProductDetailsCarousel = () => {
-  return <div>ProductDetailsCarousel</div>;
+  return (
+    <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+      <Carousel
+        infiniteLoop={true}
+        thumbWidth={60}
+        showIndicators={false}
+        showStatus={false}
+        className="productCarousel"
+      >
+        <img src="/p1.png" />
+        <img src="/p2.png" />
+        <img src="/p3.png" />
+        <img src="/p4.png" />
+        <img src="/p5.png" />
+        <img src="/p6.png" />
+        <img src="/p7.png" />
+      </Carousel>
+    </div>
+  );
 };
 
 export default ProductDetailsCarousel;
