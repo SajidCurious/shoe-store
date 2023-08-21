2c979d94abf9e01fdb3f44bc50808bf3ce2bff8d
Mohammed <sajidcurious9@gmail.com>
2023-08-22 01:41:49 +0530

    diving the details page into two left and right divs

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,7 +4,20 @@ import React from "react";
 const productDetails = () => {
   return (
     <div className="w-full md:py-20">
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
+          {/* left column start */}
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+            left
+          </div>
+          {/* left column end */}
+
+          {/* right column start */}
+
+          <div className="flex-[1] py-3">right</div>
+          {/* right column end */}
+        </div>
+      </Wrapper>
     </div>
   );
 };
