188eeac9654936d832a2f82899896987223ae91a
Mohammed <sajidcurious9@gmail.com>
2023-08-23 00:46:33 +0530

    adding multi carousel

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const RelatedProducts = () => {
-  return <div>RelatedProducts</div>;
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
+      RelatedProducts
+    </div>
+  );
 };
 
 export default RelatedProducts;
