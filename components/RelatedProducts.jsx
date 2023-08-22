eadeba222055dc9c71699bcee08c20fa01903461
Mohammed <sajidcurious9@gmail.com>
2023-08-23 00:56:06 +0530

    completed making carousel in products component

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import ProductCard from "./ProductCard";
 
 const RelatedProducts = () => {
   const responsive = {
@@ -24,7 +25,23 @@ const RelatedProducts = () => {
   };
   return (
     <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
-      RelatedProducts
+      <div className="text-2xl font-bold mb-5">You Might Also Like</div>
+      <Carousel
+        responsive={responsive}
+        containerClass="-mx-[10px]"
+        itemClass="px-[10px]"
+      >
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </Carousel>
+      ;
     </div>
   );
 };
