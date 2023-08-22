a89f6bd1d0bb0cca35df158f7052d3ac18032847
Mohammed <sajidcurious9@gmail.com>
2023-08-23 03:04:07 +0530

    completed cart page

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,7 @@
 import Cartitem from "@/components/Cartitem";
 import Wrapper from "@/components/Wrapper";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Cart = () => {
@@ -57,6 +59,27 @@ const Cart = () => {
           {/* summary end  */}
         </div>
         {/* cart content end */}
+
+        <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+          <Image
+            src="/empty-cart.jpg"
+            width={300}
+            height={300}
+            className="w-[300px] md:w-[400px]"
+          />
+          <span className="text-xl font-bold">Your cart is empty</span>
+          <span className="text-center mt-4">
+            Looks like you not added anything
+            <br />
+            Go ahead and explore top categories
+          </span>
+          <Link
+            href="/"
+            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+          >
+            Continue Shopping
+          </Link>
+        </div>
       </Wrapper>
     </div>
   );
