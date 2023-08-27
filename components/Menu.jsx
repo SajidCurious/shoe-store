5e35700adae3dfd887c710781fa8cdceb42b9d0e
Mohammed <sajidcurious9@gmail.com>
2023-08-28 00:32:16 +0530

    added fetched data

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -16,7 +16,7 @@ const subMenuData = [
   { id: 4, name: "Football shoes", doc_count: 107 },
 ];
 
-const Menu = ({ showCatMenu, setShowCatMenu }) => {
+const Menu = ({ showCatMenu, setShowCatMenu, categories }) => {
   return (
     <ul className="hidden md:flex items-center gap-8 font-medium text-black">
       {data.map((item) => {
@@ -32,17 +32,17 @@ const Menu = ({ showCatMenu, setShowCatMenu }) => {
                 <BsChevronDown size={14} />
                 {showCatMenu && (
                   <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1  text-black shadow-lg">
-                    {subMenuData.map((submenu) => {
+                    {categories?.map(({ attributes: c, id }) => {
                       return (
                         <Link
-                          href="/"
-                          key={submenu.id}
+                          href={`/category/${c.slug}`}
+                          key={id}
                           onClick={() => setShowCatMenu(false)}
                         >
                           <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
-                            {submenu.name}
+                            {c.name}
+                            <span className="opacity-50 text-sm">{`(${c.products.data.length})`}</span>
                           </li>
-                          <span className="opacity-50 text-sm">78</span>
                         </Link>
                       );
                     })}
