9d51b7b22ab6644e7d8a1f02d8d779d3ec1723bf
Mohammed <sajidcurious9@gmail.com>
2023-08-27 22:53:46 +0530

    added map method for the fetched data and passed props to the product card component

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,9 @@ export default function Home({ products }) {
           </div>
           {/* heading and paragaph end */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {products?.data?.map((product) => (
+              <ProductCard key={product?.id} data={product} />
+            ))}
           </div>
         </Wrapper>
       </main>
