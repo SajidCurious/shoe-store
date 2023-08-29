162b9c72bd43cd1c789ccdc231aec041862337a9
Mohammed <sajidcurious9@gmail.com>
2023-08-30 02:01:44 +0530

    adding dynamic route method in products page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -137,10 +137,42 @@ const productDetails = () => {
           </div>
           {/* right column end */}
         </div>
-        <RelatedProducts />
+        {/* <RelatedProducts /> */}
       </Wrapper>
     </div>
   );
 };
 
 export default productDetails;
+
+export async function getStaticPaths() {
+  const category = await fetchDataFromApi("/api/categories?populate=*");
+  const paths = category?.data?.map((c) => ({
+    params: {
+      slug: c.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params: { slug } }) {
+  const category = await fetchDataFromApi(
+    `/api/categories?filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`
+  );
+
+  return {
+    props: {
+      category,
+      products,
+      slug,
+    },
+  };
+}
