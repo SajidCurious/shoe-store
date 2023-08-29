959de0d27f5016f116233954b651222b4ed3a631
Mohammed <sajidcurious9@gmail.com>
2023-08-29 23:40:37 +0530

    using getstaticpaths method in next.js to fetch data to dynamic paths

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -25,3 +25,22 @@ const category = () => {
 };
 
 export default category;
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          name: "next.js",
+        },
+      }, // See the "paths" section below
+    ],
+    fallback: true, // false or "blocking"
+  };
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const repo = await res.json();
+  return { props: { repo } };
+};
