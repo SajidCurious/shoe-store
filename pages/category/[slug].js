6a51fe13de56c0688acc454b501814b6aa841a9f
Mohammed <sajidcurious9@gmail.com>
2023-08-30 00:50:04 +0530

    added page dynamic routing for categories

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -3,22 +3,25 @@ import React from "react";
 import ProductCard from "@/components/ProductCard";
 import { fetchDataFromApi } from "@/utils/api";
 
-const category = () => {
+const category = ({ category, products, slug }) => {
   return (
     <div>
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Running Shoes
+            {category?.data?.[0]?.attributes?.name}
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
       </Wrapper>
     </div>
@@ -42,8 +45,19 @@ export const getStaticPaths = async () => {
   };
 };
 
-// export const getStaticProps = async () => {
-//   const res = await fetch("https://api.github.com/repos/vercel/next.js");
-//   const repo = await res.json();
-//   return { props: { repo } };
-// };
+export const getStaticProps = async ({ params: { slug } }) => {
+  const category = await fetchDataFromApi(
+    `/api/categories?filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+  );
+
+  return {
+    props: {
+      category,
+      products,
+      slug,
+    },
+  };
+};
