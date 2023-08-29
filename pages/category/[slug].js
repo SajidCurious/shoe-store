a35aa0f40a096137cfd7eaee015f0f58cec163c7
Mohammed <sajidcurious9@gmail.com>
2023-08-30 01:51:57 +0530

    completed adding pagination to the category page

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,40 +1,96 @@
 import Wrapper from "@/components/Wrapper";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import { fetchDataFromApi } from "@/utils/api";
+import { data } from "autoprefixer";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+useRouter;
 
 const maxResult = 3;
-const category = ({ category, products, slug }) => {
+const Category = ({ category, products, slug }) => {
+  const [pageIndex, setPageIndex] = useState(1);
+  const { query } = useRouter();
+
+  useEffect(() => {
+    setPageIndex(1);
+  }, [query]);
+
+  const { data, error, isLoading } = useSWR(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=${maxResult}`,
+    fetchDataFromApi,
+    {
+      fallbackData: products,
+    }
+  );
+
   return (
-    <div>
+    <div className="w-full md:py-20 relative">
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
             {category?.data?.[0]?.attributes?.name}
           </div>
         </div>
+
+        {/* products grid start */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          {products?.data?.map((product) => (
+          {data?.data?.map((product) => (
             <ProductCard key={product?.id} data={product} />
           ))}
           {/* <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard /> */}
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard /> */}
         </div>
+        {/* products grid end */}
+
+        {/* PAGINATION BUTTONS START */}
+        {data?.meta?.pagination?.total > maxResult && (
+          <div className="flex gap-3 items-center justify-center my-16 md:my-0">
+            <button
+              className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+              disabled={pageIndex === 1}
+              onClick={() => setPageIndex(pageIndex - 1)}
+            >
+              Previous
+            </button>
+
+            <span className="font-bold">{`${pageIndex} of ${
+              data && data.meta.pagination.pageCount
+            }`}</span>
+
+            <button
+              className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+              disabled={pageIndex === (data && data.meta.pagination.pageCount)}
+              onClick={() => setPageIndex(pageIndex + 1)}
+            >
+              Next
+            </button>
+          </div>
+        )}
+        {/* PAGINATION BUTTONS END */}
+        {isLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-white/[0.5] flex flex-col gap-5 justify-center items-center">
+            <img src="/logo.svg" width={150} />
+            <span className="text-2xl font-medium">Loading...</span>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
 };
 
-export default category;
+export default Category;
 
-export const getStaticPaths = async () => {
+export async function getStaticPaths() {
   const category = await fetchDataFromApi("/api/categories?populate=*");
-
-  const paths = category.data.map((c) => ({
+  const paths = category?.data?.map((c) => ({
     params: {
       slug: c.attributes.slug,
     },
@@ -44,14 +100,15 @@ export const getStaticPaths = async () => {
     paths,
     fallback: false,
   };
-};
+}
 
-export const getStaticProps = async ({ params: { slug } }) => {
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params: { slug } }) {
   const category = await fetchDataFromApi(
     `/api/categories?filters[slug][$eq]=${slug}`
   );
   const products = await fetchDataFromApi(
-    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=3`
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`
   );
 
   return {
@@ -61,4 +118,4 @@ export const getStaticProps = async ({ params: { slug } }) => {
       slug,
     },
   };
-};
+}
