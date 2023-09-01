9544c696f37df489679d4115df4ee2f8635d838e
Mohammed <sajidcurious9@gmail.com>
2023-09-02 00:33:52 +0530

    imported store and provider to use Redux in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import Head from "next/head";
+import store from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
