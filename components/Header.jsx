d194263e8f1d479e30b85ad06b5725bd7827cb13
Mohammed <sajidcurious9@gmail.com>
2023-09-07 01:20:17 +0530

    added cart products number to be listed on top of icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,6 +8,7 @@ import { BsCart } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { VscChromeClose } from "react-icons/vsc";
 import { fetchDataFromApi } from "@/utils/api";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -15,6 +16,8 @@ const Header = () => {
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
   const [categories, setCategories] = useState(null);
+
+  const { cartItems } = useSelector((state) => state.cart);
 
   const controlNavbar = () => {
     if (window.scrollY > 200) {
@@ -79,9 +82,11 @@ const Header = () => {
           <Link href="/cart">
             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.06] cursor-pointer relative">
               <BsCart className="text-[15px] md:text-[20px]" />
-              <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-                5
-              </div>
+              {cartItems.length > 0 && (
+                <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                  {cartItems.length}
+                </div>
+              )}
             </div>
           </Link>
           {/* icon end */}
