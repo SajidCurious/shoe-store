b04e38fc0a89810194b3c74e06961664925368a7
Mohammed <sajidcurious9@gmail.com>
2023-09-07 00:59:37 +0530

    added functionality so that if already existing product got added again the quantity and price gets doubled

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -9,6 +9,8 @@ export const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = state.cartItems.find((p) => p.id === action.payload.id);
       if (item) {
+        item.quantity++;
+        item.attributes.price = item.oneQuantityPrice * item.quantity;
       } else {
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
