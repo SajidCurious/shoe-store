4c7ecfc28fcf1eb6d0273cd121239ce030193c6b
Mohammed <sajidcurious9@gmail.com>
2023-09-07 01:55:33 +0530

    data which is saving in redux toolkit is sent to cartItem component in props

--- pages/cart.js
+++ pages/cart.js
@@ -10,80 +10,87 @@ const Cart = () => {
   return (
     <div className="w-full md:py-20">
       <Wrapper>
-        <>
-          {/* heading and paragraph start */}
+        {cartItems.length > 0 && (
+          <>
+            {/* heading and paragraph start */}
 
-          <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-              Shopping Cart
-            </div>
-          </div>
-
-          {/* heading and paragraph end */}
-
-          {/* cart content start */}
-
-          <div className="flex flex-col lg:flex-row gap-12 py-10">
-            {/* cart items start */}
-
-            <div className="flex-[2]">
-              <div className="text-lg font-bold">Cart Items</div>
-              <Cartitem />
-              <Cartitem />
-              <Cartitem />
-            </div>
-
-            {/* cart items end */}
-
-            {/* summary start  */}
-
-            <div className="flex-[1]">
-              <div className="text-lg font-bold">Summary</div>
-              <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-                <div className="flex justify-between">
-                  <div className="uppercase text-md md:text-lg font-medium text-black">
-                    Subtotal
-                  </div>
-                  <div className="text-md md:text-lg font-medium text-black">
-                    ₹ 19,695.00
-                  </div>
-                </div>
-                <div className="text-sm md:text-md py-5 border-t mt-5">
-                  The subtotal reflects the total price of your order, including
-                  duties and taxes, before any applicable discounts. It does not
-                  include delivery costs and international transaction fees.
-                </div>
+            <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+              <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                Shopping Cart
               </div>
-              <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-80 flex items-center gap-2 justify-center">
-                Checkout
-              </button>
             </div>
 
-            {/* summary end  */}
-          </div>
-          {/* cart content end */}
-        </>
+            {/* heading and paragraph end */}
 
-        {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
-          <Image
-            src="/empty-cart.jpg"
-            width={300}
-            height={300}
-            className="w-[300px] md:w-[400px]"
-          />
-          <span className="text-xl font-bold">Your cart is empty</span>
-          <span className="text-center mt-4">
-            Looks like you not added anything
-            <br />
-            Go ahead and explore top categories
-          </span>
-          <Link
-            href="/"
-            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
-          >
-            Continue Shopping
-          </Link>
-        </div> */}
+            {/* cart content start */}
+
+            <div className="flex flex-col lg:flex-row gap-12 py-10">
+              {/* cart items start */}
+
+              <div className="flex-[2]">
+                <div className="text-lg font-bold">Cart Items</div>
+                {cartItems.map((item) => (
+                  <Cartitem key={item.key} data={item} />
+                ))}
+              </div>
+
+              {/* cart items end */}
+
+              {/* summary start  */}
+
+              <div className="flex-[1]">
+                <div className="text-lg font-bold">Summary</div>
+                <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+                  <div className="flex justify-between">
+                    <div className="uppercase text-md md:text-lg font-medium text-black">
+                      Subtotal
+                    </div>
+                    <div className="text-md md:text-lg font-medium text-black">
+                      ₹ 19,695.00
+                    </div>
+                  </div>
+                  <div className="text-sm md:text-md py-5 border-t mt-5">
+                    The subtotal reflects the total price of your order,
+                    including duties and taxes, before any applicable discounts.
+                    It does not include delivery costs and international
+                    transaction fees.
+                  </div>
+                </div>
+                <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-80 flex items-center gap-2 justify-center">
+                  Checkout
+                </button>
+              </div>
+
+              {/* summary end  */}
+            </div>
+            {/* cart content end */}
+          </>
+        )}
+
+        {/* empty cart message */}
+
+        {cartItems.length < 0 && (
+          <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+            <Image
+              src="/empty-cart.jpg"
+              width={300}
+              height={300}
+              className="w-[300px] md:w-[400px]"
+            />
+            <span className="text-xl font-bold">Your cart is empty</span>
+            <span className="text-center mt-4">
+              Looks like you not added anything
+              <br />
+              Go ahead and explore top categories
+            </span>
+            <Link
+              href="/"
+              className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+            >
+              Continue Shopping
+            </Link>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
