9c7d07a7ef6740c8c6e87bda69dd1eaef6071b12
Mohammed <sajidcurious9@gmail.com>
2023-09-07 01:33:09 +0530

    building cart page so if someone clicks on add to cart it'll get displayed in cart component

--- pages/cart.js
+++ pages/cart.js
@@ -3,64 +3,68 @@ import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <div className="w-full md:py-20">
       <Wrapper>
-        {/* heading and paragraph start */}
+        <>
+          {/* heading and paragraph start */}
 
-        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Shopping Cart
-          </div>
-        </div>
-
-        {/* heading and paragraph end */}
-
-        {/* cart content start */}
-
-        <div className="flex flex-col lg:flex-row gap-12 py-10">
-          {/* cart items start */}
-
-          <div className="flex-[2]">
-            <div className="text-lg font-bold">Cart Items</div>
-            <Cartitem />
-            <Cartitem />
-            <Cartitem />
-          </div>
-
-          {/* cart items end */}
-
-          {/* summary start  */}
-
-          <div className="flex-[1]">
-            <div className="text-lg font-bold">Summary</div>
-            <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-              <div className="flex justify-between">
-                <div className="uppercase text-md md:text-lg font-medium text-black">
-                  Subtotal
-                </div>
-                <div className="text-md md:text-lg font-medium text-black">
-                  ₹ 19,695.00
-                </div>
-              </div>
-              <div className="text-sm md:text-md py-5 border-t mt-5">
-                The subtotal reflects the total price of your order, including
-                duties and taxes, before any applicable discounts. It does not
-                include delivery costs and international transaction fees.
-              </div>
+          <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+              Shopping Cart
             </div>
-            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-80 flex items-center gap-2 justify-center">
-              Checkout
-            </button>
           </div>
 
-          {/* summary end  */}
-        </div>
-        {/* cart content end */}
+          {/* heading and paragraph end */}
 
-        <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+          {/* cart content start */}
+
+          <div className="flex flex-col lg:flex-row gap-12 py-10">
+            {/* cart items start */}
+
+            <div className="flex-[2]">
+              <div className="text-lg font-bold">Cart Items</div>
+              <Cartitem />
+              <Cartitem />
+              <Cartitem />
+            </div>
+
+            {/* cart items end */}
+
+            {/* summary start  */}
+
+            <div className="flex-[1]">
+              <div className="text-lg font-bold">Summary</div>
+              <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+                <div className="flex justify-between">
+                  <div className="uppercase text-md md:text-lg font-medium text-black">
+                    Subtotal
+                  </div>
+                  <div className="text-md md:text-lg font-medium text-black">
+                    ₹ 19,695.00
+                  </div>
+                </div>
+                <div className="text-sm md:text-md py-5 border-t mt-5">
+                  The subtotal reflects the total price of your order, including
+                  duties and taxes, before any applicable discounts. It does not
+                  include delivery costs and international transaction fees.
+                </div>
+              </div>
+              <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-80 flex items-center gap-2 justify-center">
+                Checkout
+              </button>
+            </div>
+
+            {/* summary end  */}
+          </div>
+          {/* cart content end */}
+        </>
+
+        {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
           <Image
             src="/empty-cart.jpg"
             width={300}
@@ -79,7 +83,7 @@ const Cart = () => {
           >
             Continue Shopping
           </Link>
-        </div>
+        </div> */}
       </Wrapper>
     </div>
   );
