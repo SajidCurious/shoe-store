443c7879d13ebf05a2583f60842c15cf1daec017
Mohammed <sajidcurious9@gmail.com>
2023-09-08 01:44:28 +0530

    when user changes quantity of shoes in caart the price will get updated now

--- components/Cartitem.jsx
+++ components/Cartitem.jsx
@@ -1,9 +1,21 @@
+import { updateCart } from "@/store/cartSlice";
 import Image from "next/image";
 import React from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
-
+import { useDispatch } from "react-redux";
 const Cartitem = ({ data }) => {
   const p = data.attributes;
+
+  const dispatch = useDispatch();
+
+  const updateCartItem = (e, key) => {
+    let payload = {
+      key,
+      val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
+      id: data.id,
+    };
+    dispatch(updateCart(payload));
+  };
   return (
     <div className="flex py-5 gap-3 md:gap-5 border-b">
       {/* image start  */}
@@ -49,7 +61,10 @@ const Cartitem = ({ data }) => {
           <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
             <div className="flex items-center gap-1">
               <div className="font-semibold">Size:</div>
-              <select className="hover:text-black">
+              <select
+                className="hover:text-black"
+                onChange={(e) => updateCartItem(e, "selectedSize")}
+              >
                 {p.size.data.map((item, i) => {
                   return (
                     <option
@@ -66,7 +81,10 @@ const Cartitem = ({ data }) => {
             </div>
             <div className="flex items-center gap-1">
               <div className="font-semibold">Quantity:</div>
-              <select className="hover:text-black">
+              <select
+                className="hover:text-black"
+                onChange={(e) => updateCartItem(e, "quantity")}
+              >
                 {Array.from({ length: 10 }, (_, i) => i + 1).map((q, i) => {
                   return (
                     <option key={i} value={q} selected={data.quantity === q}>
