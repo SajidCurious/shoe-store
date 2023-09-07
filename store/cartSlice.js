fe8603509aa23eb73031441ccc837fd05830b23c
Mohammed <sajidcurious9@gmail.com>
2023-09-08 01:25:05 +0530

    added updateCart function in the cartSlice.js file

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -15,10 +15,21 @@ export const cartSlice = createSlice({
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
     },
+    updateCart: (state, action) => {
+      state.cartItems = state.cartItems.map((p) => {
+        if (p.id === action.payload.id) {
+          if (action.payload.key === "quantity") {
+            p.attributes.price = p.oneQuantityPrice * action.payload.val;
+          }
+          return { ...p, [action.payload.key]: action.payload.val };
+        }
+        return p;
+      });
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, updateCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
