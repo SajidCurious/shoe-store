3f0da5ced87fef8ac3a7cb517177b93dffb95205
Mohammed <sajidcurious9@gmail.com>
2023-09-09 01:37:43 +0530

    added stripe to the cart to make payments

--- pages/cart.js
+++ pages/cart.js
@@ -4,6 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 const Cart = () => {
   const { cartItems } = useSelector((state) => state.cart);
