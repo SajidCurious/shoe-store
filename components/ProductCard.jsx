88dbd75d4eeff18c8d0d6d2249add4c495ab0788
Mohammed <sajidcurious9@gmail.com>
2024-02-13 22:33:25 +0530

    modifying

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -8,12 +8,12 @@ const ProductCard = ({ data: { attributes: p, id } }) => {
       href={`/product/${p.slug}`}
       className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
     >
-      <Image
+      {/* <Image
         width={500}
         height={500}
         src={p.thumbnail.data.attributes.url}
         alt={p.name}
-      />
+      /> */}
       <div className="p-4 text-black/[0.9]">
         <h2 className="text-lg font-medium">{p.name}</h2>
         <div className="flex items-center text-black/[0.5]">
--- pages/contact/page.js
+++
@@ -1,8 +0,0 @@
-import Contactcard from "@/components/Contactcard";
-import React from "react";
-
-const page = () => {
-  return <Contactcard />;
-};
-
-export default page;
